6df14c4762c83e4005c3bec1dc30809e0eb4f5ad
Chris Sauter <sauterdev@gmail.com>
2023-01-31 15:50:32 -0500

    react hooks exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,39 @@
-import { Component } from "react";
+import { useState } from "react";
 import FilmsList from "./components/filmsList";
 import "./App.css";
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      list: ["ready", "set", "GO"],
-      text:  ""
-    }
-
-    this.onSubmit = this.onSubmit.bind(this);
-
-  }
-
-  onSubmit(event) {
+function App(props) {
+  
+  const [list, setList] = useState(["ready", "set", "GO"]);
+  const [text, setText] = useState("");
+  
+function onSubmit(event) {
     event.preventDefault();
-    this.setState({
-      list: [...this.state.list, this.state.text]
-    })
-  }
-
-  render() {
+      setList([...list, text])
+    }
+ 
     return (
       <div>
         <h1>
           Hello World!
         </h1>
         <ul>
-          <form onSubmit={this.onSubmit}>
+          <form onSubmit={onSubmit}>
           <input type="text" name="textInput" id="textInput" 
-          value={this.state.text}
-          onChange={(event) => this.setState({
-            text: event.target.value
-          })
-        }/>
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
           <button type="submit">Add</button>
           </form>
-          {this.state.list.map((ele, index) => {
+          {list.map((ele, index) => {
             return <li key={index}>{ele}</li>
           })}
         </ul>
         <FilmsList />
       </div>
     )
-  }
+  
 }
 
 export default App;
--- src/components/filmsList.jsx
+++ src/components/filmsList.jsx
@@ -1,44 +1,29 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import "./filmsListStyle.css"
 
-class FilmsList extends Component {
-    constructor(props) {
-        super(props)
+function FilmsList(props) {
+    const [list, setList] = useState([])
 
-        this.state = {
-            list: [],
-        }
-        
-        this.getFilms = this.getFilms.bind(this);
-    }
-
-    getFilms() {
+    function getFilms() {
         let fetchFilms = fetch(`https://studioghibliapi-d6fc8.web.app/films`);
         
         fetchFilms
         .then((res) => res.json())
         .then((data) => {
-            this.setState({
-                list: data
-            })
-            
+            setList(data)   
         })
         .catch((err) => console.log(err));
     }
-
-    //Lifecycle method 2
-    componentDidMount() {
-        this.getFilms()
-    }
-
-    //Lifecycle method 1
-    render() {
-        //if the list is not empty, show the films. otherwise show "loading"
+  
+    useEffect(() => {
+        getFilms();
+    }, []) 
+ 
         return (
             <div>
-                <h1 class="text-center">Studio Ghibli Films</h1>
+                <h1 className="text-center">Studio Ghibli Films</h1>
         <ul className="tiles">
-            {this.state.list.map((ele) => {
+            {list.map((ele) => {
                 return <li key={ele.id}>
                     <h2>{ele.title}</h2>
                     <img src= {`${ele.image}`} alt="Film Poster"/>
@@ -46,7 +31,7 @@ class FilmsList extends Component {
             })}
         </ul>
         </div>
-    )}
+    )
 }
 
 
